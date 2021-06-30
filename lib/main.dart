551fda871a8f1595e057958e23df39ccf4c0653e
zhaozhentao <qq344696734@gmail.com>
2021-06-30 11:52:42 +0800

    ✨引用一个外部package

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -139,6 +140,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 onPressed: () {
                   Navigator.pushNamed(context, "arg", arguments: {"text": "t"});
                 }),
+            RandomWordsWidget(),
           ],
         ),
       ),
@@ -192,6 +194,18 @@ class TipRoute extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class RandomWordsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // 生成随机字符串
+    final wordPair = new WordPair.random();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Text(wordPair.toString()),
     );
   }
 }
